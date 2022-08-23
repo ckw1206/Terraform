d886ac3ab9e1c1f3f3af068473557a4df174d025
Kyle Wang <kyle.ck.wang@kyndryl.com>
2022-08-23 14:26:17 +0800

    update

--- Azure/AzSig.tf
+++ Azure/AzSig.tf
@@ -15,31 +15,31 @@ resource "azurerm_shared_image_gallery" "sig" {
 #Creates image definition
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/shared_image
 resource "azurerm_shared_image" "WinSrv2019" {
-  name                = "${var.img_WinSrv2019std[0]}"
+  name                = var.img_WinSrv2019std[0]
   gallery_name        = azurerm_shared_image_gallery.sig.name
   resource_group_name = azurerm_resource_group.sigrg.name
   location            = azurerm_resource_group.sigrg.location
   hyper_v_generation  = "V2"
-  os_type             = "${var.img_WinSrv2019std[1]}"
+  os_type             = var.img_WinSrv2019std[1]
 
   identifier {
-    publisher = "${var.img_WinSrv2019std[2]}"
-    offer     = "${var.img_WinSrv2019std[3]}"
-    sku       = "${var.img_WinSrv2019std[4]}"
+    publisher = var.img_WinSrv2019std[2]
+    offer     = var.img_WinSrv2019std[3]
+    sku       = var.img_WinSrv2019std[4]
   }
 }
 
 resource "azurerm_shared_image" "RHEL82" {
-  name                = "${var.img_RHEL82[0]}"
+  name                = var.img_RHEL82[0]
   gallery_name        = azurerm_shared_image_gallery.sig.name
   resource_group_name = azurerm_resource_group.sigrg.name
   location            = azurerm_resource_group.sigrg.location
   hyper_v_generation  = "V2"
-  os_type             = "${var.img_RHEL82[1]}"
+  os_type             = var.img_RHEL82[1]
 
   identifier {
-    publisher = "${var.img_RHEL82[2]}"
-    offer     = "${var.img_RHEL82[3]}"
-    sku       = "${var.img_RHEL82[4]}"
+    publisher = var.img_RHEL82[2]
+    offer     = var.img_RHEL82[3]
+    sku       = var.img_RHEL82[4]
   }
 }
--- Azure/variables.tf
+++ Azure/variables.tf
@@ -1,32 +1,32 @@
 variable "deploy_location" {
-  type        = string
-  default     = "eastus"
+  type    = string
+  default = "eastus"
 }
 
 variable "deploy_env" {
-  type        = list(string)
-  default     = ["dev"]
+  type    = list(string)
+  default = ["dev"]
 }
 
 variable "sig_resourceid" {
-  type        = string
-  default     = "/subscriptions/15460747-219f-4ef3-8606-651f1fa0d4bc/resourceGroups/Test-Kyle/providers/Microsoft.Compute/galleries/ComputeGallery/images/"
+  type    = string
+  default = "/subscriptions/15460747-219f-4ef3-8606-651f1fa0d4bc/resourceGroups/Test-Kyle/providers/Microsoft.Compute/galleries/ComputeGallery/images/"
 }
 
 variable "sig_image_def" {
-  type        = list(string)
-  default     = ["WinServer2019std","RHEL-8.2"]
+  type    = list(string)
+  default = ["WinServer2019std", "RHEL-8.2"]
 }
 
 variable "img_WinSrv2019std" {
   type        = list(string)
-  default     = ["WinSrv2019std","Windows","Kyle","WindowsServer","2019-Standard"]
+  default     = ["WinSrv2019std", "Windows", "Kyle", "WindowsServer", "2019-Standard"]
   description = "name, os-type, publisher, offer, sku"
 }
 
 variable "img_RHEL82" {
   type        = list(string)
-  default     = ["RHEL8.2","Linux","Kyle","RHEL","8.2"]
+  default     = ["RHEL8.2", "Linux", "Kyle", "RHEL", "8.2"]
   description = "name, os-type, publisher, offer, sku"
 }
 
