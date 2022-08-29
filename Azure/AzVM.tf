c591e6d5d0a7dc0eebef2fa59acc831a861814be
Kyle Wang <kyle.ck.wang@kyndryl.com>
2022-08-29 11:08:24 +0800

    update

--- Azure/AzVM.tf
+++ Azure/AzVM.tf
@@ -13,39 +13,39 @@ module "network" {
   depends_on = [azurerm_resource_group.compute]
 }
 
-# module "AzVM_Win" {
-#   for_each                      = toset(var.deploy_env)
-#   source                        = "Azure/compute/azurerm"
-#   resource_group_name           = azurerm_resource_group.compute.name
-#   is_windows_image              = true
-#   vm_hostname                   = "Win-${each.value}"
-#   admin_password                = "ComplxP@ssw0rd!"
-#   vm_os_simple                  = "WindowsServer"
-#   vnet_subnet_id                = module.network.vnet_subnets[0]
-#   vm_size                       = "Standard_B1s"
-#   nb_public_ip                  = "1"
-#   vm_os_id                      = "${var.sig_resourceid}${var.sig_image_def[0]}"
-#   vm_os_publisher               = "Kyle"
-#   vm_os_offer                   = "WindowsServer"
-#   vm_os_sku                     = "2019-Standard"
-# #  vm_os_version                 = "1.0.0"
+# module "AzVM" {
+#   for_each            = toset(var.deploy_env)
+#   source              = "git::https://github.com/ckw1206/my-terraform-azurerm-compute.git"
+#   resource_group_name = azurerm_resource_group.compute.name
+#   is_windows_image    = true
+#   vm_hostname         = "Win-${each.value}"
+#   vm_os_simple        = "WindowsServer"
+#   vnet_subnet_id      = module.network.vnet_subnets[0]
+#   vm_size             = "Standard_D2s_v3"
+#   nb_public_ip        = "1"
+#   vm_os_id            = "${var.sig_resourceid}${var.img_WinSrv2019std[0]}"
+#   vm_os_publisher     = "${var.img_WinSrv2019std[2]}"
+#   vm_os_offer         = "${var.img_WinSrv2019std[3]}"
+#   vm_os_sku           = "${var.img_WinSrv2019std[4]}"
+#   admin_password      = "ComplexP@ssw0rd"
+#   # vm_os_version       = "1.0.0"
 #   delete_os_disk_on_termination = true
 
 #   depends_on = [azurerm_resource_group.compute]
 # }
 
 # module "AzVM_Linux" {
-#   for_each                      = toset(var.deploy_env)
-#   source                        = "Azure/compute/azurerm"
-#   resource_group_name           = azurerm_resource_group.compute.name
-# #  public_ip_dns                = ["linsimplevmips"] // change to a unique name per datacenter region
-#   vm_hostname                   = "Linux-${each.value}"
-#   vnet_subnet_id                = module.network.vnet_subnets[0]
-#   nb_public_ip                  = "1"
-#   vm_os_id                      = "${var.sig_resourceid}${var.sig_image_def[1]}"
-#   vm_os_publisher               = "Kyle"
-#   vm_os_offer                   = "RHEL"
-#   vm_os_sku                     = "8.2"
+#   for_each                = toset(var.deploy_env)
+#   source                  = "git::https://github.com/ckw1206/my-terraform-azurerm-compute.git"
+#   resource_group_name     = azurerm_resource_group.compute.name
+# #  public_ip_dns          = ["linsimplevmips"] // change to a unique name per datacenter region
+#   vm_hostname             = "Linux-${each.value}"
+#   vnet_subnet_id          = module.network.vnet_subnets[0]
+#   nb_public_ip            = "1"
+#   vm_os_id                = "${var.sig_resourceid}${var.img_RHEL82[0]}"
+#   vm_os_publisher         = "${var.img_RHEL82[2]}"
+#   vm_os_offer             = "${var.img_RHEL82[3]}"
+#   vm_os_sku               = "${var.img_RHEL82[4]}"
 #   delete_os_disk_on_termination = true
 
 #   depends_on = [azurerm_resource_group.compute]
